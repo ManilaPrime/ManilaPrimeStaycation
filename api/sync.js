f534177b12a77d5fa94b6fb3a39432133dd67825
ManilaPrime <manilastaycation24@gmail.com>
2026-02-22 13:10:06 +0800

    Update Airbnb ICS links in sync.js

--- api/sync.js
+++ api/sync.js
@@ -7,17 +7,17 @@ const BRANCH = "main"
 
 const units = {
   S1TC2_595: "AIRBNB_ICS_LINK_HERE",
-  S1TC2_1567: "AIRBNB_ICS_LINK_HERE",
-  S1TC2_1569: "AIRBNB_ICS_LINK_HERE",
-  S1TC2_1583: "AIRBNB_ICS_LINK_HERE",
-  S1TC2_1585: "AIRBNB_ICS_LINK_HERE",
-  S1TC2_1586: "AIRBNB_ICS_LINK_HERE",
-  S1TC2_1839: "AIRBNB_ICS_LINK_HERE",
-  S1TD_953: "AIRBNB_ICS_LINK_HERE",
-  S1TD_963: "AIRBNB_ICS_LINK_HERE",
-  S3T1_372: "AIRBNB_ICS_LINK_HERE",
-  S3T1_525: "AIRBNB_ICS_LINK_HERE",
-  S3T1_855: "AIRBNB_ICS_LINK_HERE",
+  S1TC2_1567: "https://www.airbnb.com/calendar/ical/1600802153832922086.ics?t=c083a5d686354f0cb6948556c957c132",
+  S1TC2_1569: "https://www.airbnb.com/calendar/ical/1542649713581564854.ics?t=6d9d08b2e06a498f971b66aaf6917b22",
+  S1TC2_1583: "https://www.airbnb.com/calendar/ical/1514352264532693099.ics?t=63ee007127de4958b2fff3389e160f29",
+  S1TC2_1585: "https://www.airbnb.com/calendar/ical/1492660768873686668.ics?t=f7c603da55f24a15871ceb3f85ee3b40",
+  S1TC2_1586: "https://www.airbnb.com/calendar/ical/1456394329482405564.ics?t=56d67dff321549a88e1b1d8a81185dfb",
+  S1TC2_1839: "https://www.airbnb.com/calendar/ical/1492666082664918894.ics?t=345fa7f61fd64349ac282abc4dcc4e7d",
+  S1TD_953: "https://www.airbnb.com/calendar/ical/1575381453708843835.ics?t=c4cbd8e6fd9e4e1ca371ce847b826a97",
+  S1TD_963: "https://www.airbnb.com/calendar/ical/1550363204882077789.ics?t=480c2289ebd649e690ae76ab62d2b49f",
+  S3T1_372: "https://www.airbnb.com/calendar/ical/1550714503840063391.ics?t=8c87fe6c5145401fa2f6a37ddb3bb2e2",
+  S3T1_525: "https://www.airbnb.com/calendar/ical/1573584451241601621.ics?t=50178d21eec548e5a8ca38862a3ecc55",
+  S3T1_855: "https://www.airbnb.com/calendar/ical/1521108058648348925.ics?t=ddc07a9f4efb45d7bc55fcd79084690d",
   bedspace: "AIRBNB_ICS_LINK_HERE"
 }
 
