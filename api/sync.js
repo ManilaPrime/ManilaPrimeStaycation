3453ffec5deb38fa4b37b76b8fc22ab5c11a5a3b
ManilaPrime <manilastaycation24@gmail.com>
2026-02-22 14:01:43 +0800

    Add new calendar URLs for S1TC2_1537 and S2T1_1407

--- api/sync.js
+++ api/sync.js
@@ -7,6 +7,7 @@ const BRANCH = "main"
 
 const units = {
   S1TC2_595: "https://www.airbnb.com/calendar/ical/1530361023768759063.ics?t=baffb130590a438cac29735b143b96fe&locale=en-GB",
+  S1TC2_1537: "https://www.airbnb.com/calendar/ical/1625354607063896867.ics?t=fc2e3b1d4d6a4eb58cdfebf6da9f5339",
   S1TC2_1567: "https://www.airbnb.com/calendar/ical/1600802153832922086.ics?t=c083a5d686354f0cb6948556c957c132",
   S1TC2_1569: "https://www.airbnb.com/calendar/ical/1542649713581564854.ics?t=6d9d08b2e06a498f971b66aaf6917b22",
   S1TC2_1583: "https://www.airbnb.com/calendar/ical/1514352264532693099.ics?t=63ee007127de4958b2fff3389e160f29",
@@ -15,6 +16,7 @@ const units = {
   S1TC2_1839: "https://www.airbnb.com/calendar/ical/1492666082664918894.ics?t=345fa7f61fd64349ac282abc4dcc4e7d",
   S1TD_953: "https://www.airbnb.com/calendar/ical/1575381453708843835.ics?t=c4cbd8e6fd9e4e1ca371ce847b826a97",
   S1TD_963: "https://www.airbnb.com/calendar/ical/1550363204882077789.ics?t=480c2289ebd649e690ae76ab62d2b49f",
+  S2T1_1407: "https://www.airbnb.com/calendar/ical/1620725893625438160.ics?t=bb767e49f86443c4a162dccb2ca09763",
   S3T1_372: "https://www.airbnb.com/calendar/ical/1550714503840063391.ics?t=8c87fe6c5145401fa2f6a37ddb3bb2e2",
   S3T1_525: "https://www.airbnb.com/calendar/ical/1573584451241601621.ics?t=50178d21eec548e5a8ca38862a3ecc55",
   S3T1_855: "https://www.airbnb.com/calendar/ical/1521108058648348925.ics?t=ddc07a9f4efb45d7bc55fcd79084690d",
